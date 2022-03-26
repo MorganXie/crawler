c2c8e3627f14a92e384472912b164563af62f020
MorganXie <529303860@qq.com>
2022-03-27 01:35:51 +0800

    初步完成爬虫算法 (#2)
    * 初步完成爬虫算法
    
    * 删除WhitespaceAfter校验
    
    * 重构一波

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,23 +1,80 @@
 package com.github.hcsp;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet("https://sina.cn");
-            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                System.out.println(response1.getStatusLine());
-                HttpEntity entity1 = response1.getEntity();
-                System.out.println(EntityUtils.toString(entity1));
-//                EntityUtils.consume(entity1);
+        List<String> linkPool = new ArrayList<>();
+        Set<String> processedLinks = new HashSet<>();
+        linkPool.add("https://sina.cn/");
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+            String link = linkPool.remove(linkPool.size() - 1);
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                doc.select("a").stream().map(eachTag->eachTag.attr("href")).forEach(linkPool::add);
+                storeIntoDatabaseIfItIsNewsPage(doc);
+                processedLinks.add(link);
             }
         }
+
+
+    }
+
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                System.out.println(articleTag.child(0).text());
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        System.out.println(link);
+        if (link.startsWith("//")) {
+            link += "htttps:";
+            System.out.println(link);
+        }
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        HttpEntity entity1 = response1.getEntity();
+        String html = EntityUtils.toString(entity1);
+        return Jsoup.parse(html);
+    }
+
+
+    private static boolean isInterestingLink(String link) {
+        return isNewsPage(link) || isIndexPage(link) && isNotLoginPage(link);
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
+    }
+
+    private static boolean isIndexPage(String link) {
+        return link.equals("https://sina.cn/");
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
     }
 }
