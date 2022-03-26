59055743a15995b1ca8ca1a4aa6d78714afee804
MorganXie <529303860@qq.com>
2022-03-27 01:26:35 +0800

    重构一波

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,4 +1,5 @@
 package com.github.hcsp;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -8,8 +9,10 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         List<String> linkPool = new ArrayList<>();
@@ -19,43 +22,59 @@ public class Main {
             if (linkPool.isEmpty()) {
                 break;
             }
-            String link = linkPool.remove(linkPool.size()-1);
+            String link = linkPool.remove(linkPool.size() - 1);
             if (processedLinks.contains(link)) {
                 continue;
             }
-            if (link.contains("news.sina.cn")||link.equals("https://sina.cn/")) {
-                System.out.println(link);
-                if (link.startsWith("//")){
-                    link+="htttps:";
-                    System.out.println(link);
-                }
-                try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-                    HttpGet httpGet = new HttpGet(link);
-                    httpGet.addHeader("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36");
-                    try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                        HttpEntity entity1 = response1.getEntity();
-                        String html = EntityUtils.toString(entity1);
-                        Document doc = Jsoup.parse(html);
-                        ArrayList<Element> links = doc.select("a");
-                        for (Element aTag : links) {
-                            if (!aTag.attr("href").equals("javascript:;")){
-                                linkPool.add(aTag.attr("href"));
-                            }
-                        }
-                        ArrayList<Element> articleTags = doc.select("article");
-                        if (!articleTags.isEmpty()){
-                            for (Element articleTag:articleTags) {
-                                System.out.println(articleTag.child(0).text());
-                            }
-                        }
-                    }
-                }
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                doc.select("a").stream().map(eachTag->eachTag.attr("href")).forEach(linkPool::add);
+                storeIntoDatabaseIfItIsNewsPage(doc);
                 processedLinks.add(link);
-            } else {
-                continue;
             }
         }
 
 
+    }
+
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                System.out.println(articleTag.child(0).text());
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        System.out.println(link);
+        if (link.startsWith("//")) {
+            link += "htttps:";
+            System.out.println(link);
+        }
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        HttpEntity entity1 = response1.getEntity();
+        String html = EntityUtils.toString(entity1);
+        return Jsoup.parse(html);
+    }
+
+
+    private static boolean isInterestingLink(String link) {
+        return isNewsPage(link) || isIndexPage(link) && isNotLoginPage(link);
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
+    }
+
+    private static boolean isIndexPage(String link) {
+        return link.equals("https://sina.cn/");
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
     }
 }
